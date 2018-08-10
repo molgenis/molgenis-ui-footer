4a3e469f1fdeb2fd241fbf3ad83b46fb18c6d246
sido <sido@haakma.org>
2018-08-10 15:11:55 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import MolgenisFooter from './MolgenisFooter'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#molgenis-footer',
+  template: '<MolgenisFooter />',
+  components: {MolgenisFooter}
+})
